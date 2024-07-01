866cce0958afa1e26977395b58f5700852673457
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-07-02 00:49:37 +0800

    modified horizontal padding

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,9 +5,7 @@ const Layout = ({ children }) => {
 	return (
 		<div className="font-lato">
 			<Navbar />
-			<div className="bg-gray-900 px-4 lg:px-32 w-full text-gray-300">
-				{children}
-			</div>
+			<div className="bg-gray-900 w-full text-gray-300">{children}</div>
 		</div>
 	);
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,10 +13,9 @@ const Navbar = () => {
 
 	return (
 		<div className="shadow-md w-full fixed top-0 left-0">
-			<div className="md:flex items-center justify-between bg-gray-900 py-4 md:px-10 px-7">
+			<div className="md:flex items-center justify-between bg-gray-900 py-4 md:px-10 px-4">
 				{/* logo section */}
 				<div className="font-bold text-green-400 text-2xl cursor-pointer flex items-center gap-1">
-					{/* <RocketLaunchIcon className="w-7 h-7 text-blue-600" /> */}
 					<span>kurt.vincent</span>
 				</div>
 				{/* Menu icon */}
@@ -29,7 +28,7 @@ const Navbar = () => {
 				{/* linke items */}
 				<ul
 					className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-						open ? 'top-12 mt-12 bg-gray-800' : 'top-[-490px]'
+						open ? 'top-12 mt-12 bg-gray-900' : 'top-[-490px]'
 					}`}
 				>
 					{Links.map((link) => (
