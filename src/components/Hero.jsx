0f9f51e08e117d19e520ec6689ce8dfe80eb8988
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-07-02 00:49:04 +0800

    added bg image in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { CodeBracketIcon } from '@heroicons/react/16/solid';
 const Hero = () => {
 	return (
 		<section id="hero">
-			<div className="flex justify-center items-center h-screen">
+			<div className="hero-section flex justify-center items-center h-screen">
 				<div className="flex w-full gap-x-16 justify-center items-center ">
 					<div className="flex flex-col items-center gap-y-4">
 						<p className="text-lg md:text-xl lg:text-3xl">Hello there! I'm</p>
@@ -15,17 +15,18 @@ const Hero = () => {
 						<hr />
 						<div className="flex justify-center items-center gap-x-2">
 							<CodeBracketIcon className="w-8 h-8 text-green-400" />
-							<h3 className="text-lg md:text-xl lg:text-3xl">Web Developer</h3>
+							<h3 className="text-lg md:text-xl lg:text-3xl">
+								Software Developer
+							</h3>
 							<CodeBracketIcon className="w-8 h-8 text-green-400" />
 						</div>
 						<button
 							type="button"
-							class="w-48 text-green-400 bg-gray-900 border border-green-400 focus:outline-none hover:bg-gray-800 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+							class="w-48 text-gray-200 bg-gray-900 border border-green-400 focus:outline-none hover:bg-green-400 hover:text-gray-900 font-medium rounded-xl text-xs px-5 py-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
 						>
 							DOWNLOAD RESUME
 						</button>
 					</div>
-					{/* <img className="" src={Profile} alt="profile-pic.png" width={400} /> */}
 				</div>
 			</div>
 		</section>
