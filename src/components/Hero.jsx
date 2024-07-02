1ab1183ce92b67a8862d81fdc9fdcb37aee9d7ac
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2024-07-02 15:11:49 +0800

    updated about description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
 				<div className="flex w-full gap-x-16 justify-center items-center ">
 					<div className="flex flex-col items-center gap-y-4">
 						<p className="text-lg md:text-xl lg:text-3xl">Hello there! I'm</p>
-						<h1 className="text-center font-bold text-green-400 text-6xl md:text-7xl lg:text-8xl">
+						<h1 className="text-center font-bold text-green-400 text-6xl md:text-7xl lg:text-8xl ">
 							Kurt Vincent Timajo
 						</h1>
 						<hr />
@@ -22,7 +22,7 @@ const Hero = () => {
 						</div>
 						<button
 							type="button"
-							class="w-48 text-gray-200 bg-gray-900 border border-green-400 focus:outline-none hover:bg-green-400 hover:text-gray-900 font-medium rounded-xl text-xs px-5 py-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+							class="w-48 text-gray-200 bg-gray-900 border border-green-400 focus:outline-none hover:shadow-md hover:shadow-green-400 font-medium rounded-xl text-xs px-5 py-2 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
 						>
 							DOWNLOAD RESUME
 						</button>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,17 +7,18 @@ const About = () => {
 			<div className="py-24 px-4 lg:px-32">
 				<SectionTitle title={'about me'} />
 				<div className="flex flex-col bg-gray-800 rounded-xl">
-					<div className="flex flex-col lg:flex-row gap-x-16 justify-center items-center py-6 lg:py-16 px-6 lg:px-12">
-						<img src={AboutPic} alt="" className="w-48 lg:w-64 mb-6 lg:mb-0" />
+					<div className="flex flex-col lg:flex-row gap-x-16 justify-center items-center py-6 lg:py-8 px-6 lg:px-12">
+						<img src={AboutPic} alt="" className="w-48 lg:w-96 mb-6 lg:mb-0 " />
 						<div className="flex flex-col gap-y-6 text-center lg:text-left">
-							<p className="text-xl">
-								I'm <span className="text-green-400">Kurt Vincent Timajo</span>,
-								a <span className="text-green-400">software developer</span>{' '}
-								based in Cagayan de Oro City, Philippines. I specialize in{' '}
+							<p className="text-2xl">
+								Hey there! I'm{' '}
+								<span className="text-green-400">Kurt Vincent Timajo</span>, a{' '}
+								<span className="text-green-400">software developer</span> based
+								in the lively Cagayan de Oro City, Philippines specializing in{' '}
 								<span className="text-green-400">full stack development</span>{' '}
-								utilizing Javascript frameworks such as ReactJS, and NodeJS. I
-								am dedicated to bringing creative ideas to life through building
-								websites, web and mobile apps.
+								using Javascript frameworks such as ReactJS, and NodeJS. I love
+								mixing creativity with code to create practical and innovative
+								solutions.
 							</p>
 						</div>
 					</div>
